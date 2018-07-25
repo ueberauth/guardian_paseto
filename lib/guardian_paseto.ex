36fba9cb8201a88674085b2d58ab1488a9d49f4b
Ian Clark <ian@ianleeclark.com>
2018-07-24 20:14:52 -0500

    Initial commit containing skeleton and license

---
+++ lib/guardian_paseto.ex
@@ -0,0 +1,33 @@
+defmodule GuardianPaseto do
+  @moduledoc """
+  Documentation for GuardianPaseto.
+  """
+  alias Guardian.Token
+
+  def build_claims(mod, resource, sub, claims, opts) do
+  end
+
+  def create_claims(mod, claims, opts) do
+  end
+
+  def decode_token(mod, token, opts) do
+  end
+
+  def exchange(mod, token, from_type, to_type, opts) do
+  end
+
+  def peek(mod, token) do
+  end
+
+  def refresh(mod, old_token, opts) do
+  end
+
+  def revoke(mod, claims, token, opts) do
+  end
+
+  def token_id() do
+  end
+
+  def verify_claims(mod, claims, opts) do
+  end
+end
---
+++ test/guardian_paseto_test.exs
@@ -0,0 +1,8 @@
+defmodule GuardianPasetoTest do
+  use ExUnit.Case
+  doctest GuardianPaseto
+
+  test "greets the world" do
+    assert GuardianPaseto.hello() == :world
+  end
+end
