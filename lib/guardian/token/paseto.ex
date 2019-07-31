8d1c8988c9dd54bf7ecc84e179fece962d74701d
yoshitaka_takeuchi <6265686+yosatak@users.noreply.github.com>
2019-08-01 01:27:34 +0900

    Resolve variable rebind

--- lib/guardian/token/paseto.ex
+++ lib/guardian/token/paseto.ex
@@ -56,8 +56,7 @@ defmodule Guardian.Token.Paseto do
           | Guardian.Token.secret_error()
   def create_token(mod, claims, opts) do
     key = secret_key(mod, opts)
-    claims = Poison.encode!(claims)
-    do_create_token(mod, claims, opts, key)
+    do_create_token(mod, claims |> Poison.encode!, opts, key)
   end
 
   @doc """
